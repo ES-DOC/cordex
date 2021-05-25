6f831187ec176f3f22d3032db1dd75937bbf4130
Mark A. Greenslade <mark@casperlabs.io>
2021-05-25 10:06:26 +0400

    1. Added atmos.

--- lib/utils/vocabs.py
+++ lib/utils/vocabs.py
@@ -96,8 +96,6 @@ def yield_topics(institution_id):
     for i in get_institutes(institution_id):
         for m in get_models_by_institution(i):
             for t in get_topics():
-                if t.canonical_name != "toplevel":
-                    continue
                 for d in get_domains():    
                     yield i, m, t, d
 
